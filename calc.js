1830e2b4864765cbba737d6ee567ab91abfa790d
Laramie Downs <laramiedowns1994@gmail.com>
2021-11-09 16:50:29 -0600

    Exponent implemntation and explaination added and working

--- calc.js
+++ calc.js
@@ -7,12 +7,14 @@ const reader = readline.createInterface({
   output: process.stdout,
 });
 
-
+//Instructions on what symbols to input and format
 console.log("Addition use: + (i.e. \"+ 1 1\")")
 console.log("Subtraction use: - (i.e. \"- 2 1\")") 
 console.log("Multiplication use: * (i.e. \"* 2 2\")") 
 console.log("Division use / (i.e. \"/ 2 2\")")
 console.log("Square Root use: # (i.e. \"# 4\")")
+console.log("Eponents use: ^ (i.e. \"^ 2 3\")")
+
 reader.question("What would you like to calculate?", function(input){
 	tokens = input.split(' ');
 	
@@ -47,6 +49,12 @@ reader.question("What would you like to calculate?", function(input){
 	//Square Root
 	else if(mathSymbol === "#"){
 		let x = Math.sqrt(num1)
+		console.log(x)
+	}
+
+	//Exponent
+	else if(mathSymbol === "^"){
+		let x = Math.pow(num1,num2);
 		console.log(x)
 	}
 	// This line closes the connection to the command line interface.
